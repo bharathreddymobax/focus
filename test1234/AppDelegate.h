3dfaecbe8a7b1d3b6845bbdac790f73aa09fe724
bharath8830 <bharathreddy.venkat@gmail.com>
2016-05-06 19:06:18 -0700

    Initial Commit

---
+++ test1234/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1234
+//
+//  Created by mobeo on 06/05/16.
+//  Copyright © 2016 mobeo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1234/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test1234
+//
+//  Created by mobeo on 06/05/16.
+//  Copyright © 2016 mobeo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
